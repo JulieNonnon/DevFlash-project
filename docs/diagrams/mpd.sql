880514de94a0d168ea89e7853b87ef702348e634
JulieNonnon <julie.nonnon@gmail.com>
2026-09-06 19:19:08 +0200

    docs(merise) : update mpd script(#16)

--- docs/diagrams/mpd.sql
+++ docs/diagrams/mpd.sql
@@ -3,6 +3,7 @@
 CREATE TABLE Language_(
    language_name VARCHAR(50),
    slug VARCHAR(50) NOT NULL,
+   created_at DATETIME NOT NULL,
    PRIMARY KEY(language_name),
    UNIQUE(slug)
 );
@@ -10,6 +11,7 @@ CREATE TABLE Language_(
 CREATE TABLE Category(
    category_name VARCHAR(20),
    description TEXT NOT NULL,
+   created_at DATETIME NOT NULL,
    language_name VARCHAR(50) NOT NULL,
    PRIMARY KEY(category_name),
    FOREIGN KEY(language_name) REFERENCES Language_(language_name)
@@ -26,17 +28,31 @@ CREATE TABLE Flashcard(
    FOREIGN KEY(category_name) REFERENCES Category(category_name)
 );
 
+CREATE TABLE Role(
+   role_name VARCHAR(50),
+   created_at DATETIME NOT NULL,
+   PRIMARY KEY(role_name)
+);
+
 CREATE TABLE User_(
    email VARCHAR(320),
    username VARCHAR(50) NOT NULL,
    password VARCHAR(255) NOT NULL,
-   role ENUM('user', 'admin') NOT NULL,
    created_at DATETIME NOT NULL,
+   role_name VARCHAR(50) NOT NULL,
    PRIMARY KEY(email),
-   UNIQUE(password)
+   FOREIGN KEY(role_name) REFERENCES Role(role_name)
 );
 
 CREATE TABLE Favoriser(
+   ref_flashcard VARCHAR(20),
+   email VARCHAR(320),
+   PRIMARY KEY(ref_flashcard, email),
+   FOREIGN KEY(ref_flashcard) REFERENCES Flashcard(ref_flashcard),
+   FOREIGN KEY(email) REFERENCES User_(email)
+);
+
+CREATE TABLE Comprendre(
    ref_flashcard VARCHAR(20),
    email VARCHAR(320),
    PRIMARY KEY(ref_flashcard, email),
